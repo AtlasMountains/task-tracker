232aa0c248b923cead3c048d45142c17dbd00035
Atlas <99806895+AtlasMountains@users.noreply.github.com>
2022-11-01 14:56:52 +0100

    task: prevent selecting text, change cursor pointer

--- src/components/Task.js
+++ src/components/Task.js
@@ -3,7 +3,7 @@ import { FaTimes } from "react-icons/fa";
 const Task = ({ task, onDelete, onToggle }) => {
   return (
     <li
-      className={`flex justify-between content-center pl-2 px-3 py-1 ${
+      className={`flex justify-between content-center pl-2 px-3 py-1 select-none ${
         task.reminder ? "bg-amber-500" : "bg-slate-200"
       } border-t-2 border-black`}
       onDoubleClick={() => onToggle(task.id)}
@@ -13,7 +13,7 @@ const Task = ({ task, onDelete, onToggle }) => {
         <h4 className="pl-2 text-sm">{task.day}</h4>
       </div>
       <div className="text-red-500 my-auto">
-        <FaTimes onClick={() => onDelete(task.id)} />
+        <FaTimes onClick={() => onDelete(task.id)} className="cursor-pointer" />
       </div>
     </li>
   );
