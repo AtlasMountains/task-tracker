30a63f6b43b715e1f1c3fccc432fcd5c2adb9d7b
Atlas <99806895+AtlasMountains@users.noreply.github.com>
2022-11-01 12:41:55 +0100

    extracting task into seperate component

---
+++ src/components/Task.js
@@ -0,0 +1,17 @@
+import { FaTimes } from "react-icons/fa";
+
+const Task = ({ task }) => {
+  return (
+    <li className="flex justify-between content-center pl-2 px-3 py-1 bg-slate-200 border-t-2 border-black">
+      <div>
+        <h3 className="font-semibold">{task.text}</h3>
+        <h4 className="pl-2 text-sm">{task.day}</h4>
+      </div>
+      <div className="text-red-500 my-auto">
+        <FaTimes />
+      </div>
+    </li>
+  );
+};
+
+export default Task;
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,14 +1,10 @@
+import Task from "./Task";
+
 const Tasks = ({ Tasks }) => {
   return (
     <ul className="">
       {Tasks.map((task) => (
-        <li
-          className="pl-2 py-1 bg-slate-200 border-t-2 border-black"
-          key={task.id}
-        >
-          <h3 className="font-semibold">{task.text}</h3>
-          <h4 className="pl-2 text-sm">{task.day}</h4>
-        </li>
+        <Task task={task} key={task.id} />
       ))}
     </ul>
   );
